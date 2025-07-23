2b71bb0c02a1b4036ad037c9a6b72f7914468086
Alex <alex.lam.france@gmail.com>
2025-07-23 17:39:36 +0700

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,15 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+@extends('layouts.base')
+
+@section('content')
+<div class="container py-5">
+
+
+ <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
 
-    <div class="py-12">
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
@@ -13,5 +17,12 @@
                 </div>
             </div>
         </div>
+        </div>
     </div>
-</x-app-layout>
+
+    {{-- Optional bottom content --}}
+    <div class="text-center text-white mt-5 small">
+        Développé avec ❤️ par toi, propulsé par Laravel.
+    </div>
+</div>
+@endsection
